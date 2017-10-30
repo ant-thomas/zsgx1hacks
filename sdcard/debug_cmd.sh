09e738056196f303caa7afd770bca109e72cb610
ant-thomas <anthonythomas@gmail.com>
2017-10-30 09:22:56 +0000

    Update debug_cmd.sh

--- sdcard/debug_cmd.sh
+++ sdcard/debug_cmd.sh
@@ -5,7 +5,11 @@
 cp /bin/busybox /bin/busybox-orig
 # copies new busybox binary
 cp /mnt/busybox-armv6l /bin/busybox
+
+# makes backup of original hosts file
 cp /etc/hosts /etc/hosts.orig
+# copies new hosts file blocking cloud services
 cp /mnt/hosts.new /etc/hosts
+
 # update root password to root - login via telnet now possible
 (sleep 20 && echo "root:o.eyOMtPAPfbg:0:0:root:/root/:/bin/sh" > /etc/passwd && cat /etc/passwd ) &
