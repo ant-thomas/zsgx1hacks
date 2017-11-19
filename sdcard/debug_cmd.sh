6b4a1419540280aa2ea8046a98a1e6c9ac6384cf
ant-thomas <anthonythomas@gmail.com>
2017-11-19 12:59:31 +0000

    Update debug_cmd.sh

--- sdcard/debug_cmd.sh
+++ sdcard/debug_cmd.sh
@@ -12,24 +12,22 @@ cp /mnt/busybox-armv6l /bin/busybox
 cp /mnt/dropbearmulti /bin/dropbearmulti
 mkdir /etc/dropbear
 cp /mnt/dropbear_ecdsa_host_key /etc/dropbear/dropbear_ecdsa_host_key
-/bin/dropbearmulti dropbear                                          
+/bin/dropbearmulti dropbear
 
 # update hosts file to prevent communication
 cp /mnt/hosts.new /etc/hosts
 
-# wifi creds
+# wifi creds - currently doesn't work
 #cp /mnt/wpa_supplicant.conf /home/wpa_supplicant.conf
 
 # update root password to root
 (sleep 20 && echo "root:o.eyOMtPAPfbg:0:0:root:/root/:/bin/sh" > /etc/passwd && cat /etc/passwd ) &
 
-# update wifi creds
-(sleep 20 && /mnt/mmc01/0/goke_p2pcam_param --wifissid=SSID --wifipass=WIFIPASSKEY ) &
-
-# turn off high pitched noise
-(sleep 20 && /mnt/mmc01/0/goke_volume -s 0 ) &                 
-
 # update shadow password file
 (sleep 20 && cp /mnt/mmc01/0/shadow /etc/shadow ) &
 
+# update wifi creds - currently doesn't work
+#(sleep 20 && /mnt/mmc01/0/goke_p2pcam_param --wifissid=SSID --wifipass=WIFIPASSKEY ) &
 
+# turn off high pitched noise
+(sleep 20 && /mnt/mmc01/0/goke_volume -s 0 ) &
