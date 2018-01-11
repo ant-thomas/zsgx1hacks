42fb1124f4535886440d3639f49ec0652fdbde95
Kristiyan Tsaklev <cuki@cukii.tk>
2018-01-11 12:15:43 +0200

    Look for the files in the dir of the script

--- sdcard/debug_cmd.sh
+++ sdcard/debug_cmd.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 #
 
-cp /mnt/group /etc/group
+DIR="$(dirname "$0")"
+
+cp "$DIR"/group /etc/group
 
 # install updated version of busybox
 cp /bin/busybox /bin/busybox-orig
-cp /mnt/busybox-armv6l /bin/busybox
+cp "$DIR"/busybox-armv6l /bin/busybox
 /bin/busybox --install -s
 
 # setup and install dropbear ssh server
-cp /mnt/dropbearmulti /bin/dropbearmulti
+cp "$DIR"/dropbearmulti /bin/dropbearmulti
 mkdir /etc/dropbear
-cp /mnt/dropbear_ecdsa_host_key /etc/dropbear/dropbear_ecdsa_host_key
+cp "$DIR"/dropbear_ecdsa_host_key /etc/dropbear/dropbear_ecdsa_host_key
 /bin/dropbearmulti dropbear
 
 # update hosts file to prevent communication
-cp /mnt/hosts.new /etc/hosts
+cp "$DIR"/hosts.new /etc/hosts
 
 # update the time
 ntpd -q -p uk.pool.ntp.org
 
 # wifi creds - currently doesn't work
-#cp /mnt/wpa_supplicant.conf /home/wpa_supplicant.conf
+#cp "$DIR"/wpa_supplicant.conf /home/wpa_supplicant.conf
 
 # update wifi creds - currently doesn't work
-#(sleep 20 && /mnt/mmc01/0/goke_p2pcam_param --wifissid=SSID --wifipass=WIFIPASSKEY ) &
+#(sleep 20 && "$DIR"/goke_p2pcam_param --wifissid=SSID --wifipass=WIFIPASSKEY ) &
 
 # turn off high pitched noise
-(sleep 20 && /mnt/mmc01/0/goke_volume -s 0 ) &
+(sleep 20 && "$DIR"/goke_volume -s 0 ) &
